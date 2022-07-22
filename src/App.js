cfad9b41495dac7cb024e81f25d82e4496e9c5ca
msparth89@gmail.com-at-707163464977 <msparth89@gmail.com>
2022-07-22 15:18:49 +0530

    added code execution query

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import camera from './Camera';
 import React, { useState, useEffect } from "react";
 import Header from './Header';
 import Footer from './Footer';
+import axios from 'axios';
 
 function App() {
-const [Loginstatus,setLoginStatus]=useState(false);
+  const [Loginstatus, setLoginStatus] = useState(false);
 
+  const [Gotres, setGotres] = useState(false);
+  const [ResData, setResData] = useState("");
   // camera.startCamera();
-
-  function isAuthorizedUser(email){
-    if(email == "madhavi.c@gradious.com"){
+  function isAuthorizedUser(email) {
+    if (email == "madhavi.c@gradious.com") {
       setLoginStatus(true);
     }
   }
-
-
   useEffect(() => {
 
     /* global google*/
@@ -30,10 +30,7 @@ const [Loginstatus,setLoginStatus]=useState(false);
       { theme: "outline", size: "large" }  // customization attributes
     );
     google.accounts.id.prompt(); // also display the One Tap dialog
-
-
     function handleCredentialResponse(response) {
-
       console.log("Encoded JWT ID token: " + response.credential);
       /*global jwt_decode*/
       const responsePayload = jwt_decode(response.credential);
@@ -49,33 +46,36 @@ const [Loginstatus,setLoginStatus]=useState(false);
       isAuthorizedUser(responsePayload.email);
 
     }
-
-    
-
-
-
-
-
-
-
-
-    // const intervalId = setInterval(() => {
-    //   camera.takeSnapshot(); setsent(true);
-    // }, 5000);
-    // return () => clearInterval(intervalId);
-
-    
   }, []);
 
-
+  const getApiData = () => {
+    const requestOptions = {
+      method: 'POST',
+      body: JSON.stringify({ "code": "public class TestClass {public static void main(String[] args) {System.out.println(1+2);System.out.println(9+9);}}" })
+    };
+    fetch("https://pk8eaiaa0h.execute-api.ap-south-1.amazonaws.com/beta", requestOptions)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+        },
+        (error) => {
+          console.log(error)
+        }
+      )
+  };
 
   return (
     <div className="App">
-    {!Loginstatus&& <div id="buttonDiv"></div>}
+      {!Loginstatus && <div id="buttonDiv"></div>}
 
-   {Loginstatus && <Header/>} 
-      
-   {Loginstatus && <Footer/>} 
+      {Loginstatus && <Header />}
+
+      {Loginstatus && <Footer />}
+
+      <button onClick={() => getApiData()}> get result</button>
+
+      {Gotres && <p>ResData</p>}
 
     </div>
   );
