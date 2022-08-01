99a31270b7c1758e75f30755bb3e7c14f4c09df4
msparth89@gmail.com-at-707163464977 <msparth89@gmail.com>
2022-08-01 16:32:04 +0530

    updated httpcall

--- src/App.js
+++ src/App.js
@@ -4,31 +4,31 @@ import camera from './Camera';
 import React, { useState, useEffect } from "react";
 import Header from './Header';
 import Problems from './Problems';
-import CodeEditor from './CodeEditor'
-import OutputWindow from './OutputWindow'
+import CodeEditor from './CodeEditor';
+import OutputWindow from './OutputWindow';
 import Footer from './Footer';
 import axios from 'axios';
 import Split from 'react-split';
+import { executeUserCode,buildCodeForJava,httpCall } from './util';
+//import executeCode from './util';
 
 function App() {
   const [Loginstatus, setLoginStatus] = useState(false);
   const [Gotres, setGotres] = useState(false);
-  var [UserCode, SetUserCode] = useState(``);
-  var [CodeOutput, SetCodeOutput] = useState(``);
+  var [UserCode, setUserCode] = useState(``);
+  var [CodeOutput, setCodeOutput] = useState(``);
 
   function codeChange(NewValue) {
-    SetUserCode(NewValue)
+    setUserCode(NewValue);
   }
   // camera.startCamera();
   function isAuthorizedUser(email) {
     //if (email == "msparth89@gmail.com") {
-      setLoginStatus(true);
+    setLoginStatus(true);
     //}
   }
   useEffect(() => {
-
     /* global google*/
-
     google.accounts.id.initialize({
       client_id: "197504797372-j73fmcvfadrrqokndu07vat9eobuh6sj.apps.googleusercontent.com",
       callback: handleCredentialResponse
@@ -50,67 +50,68 @@ function App() {
       console.log("Image URL: " + responsePayload.picture);
       //document.getElementById('profile').innerHTML = `<img src="${responsePayload.picture}" class="profile">`;
       console.log("Email: " + responsePayload.email);
-
       isAuthorizedUser(responsePayload.email);
-
-      SetUserCode(`public class TestClass
-      {
-          public static void main(String[] args) 
-          { 
-          //start here
-          
-          }
-      }`)
-
     }
   }, []);
 
+   async function executeCode(language,method,code)
+  {
 
-  function getOutput() {
-    var code = UserCode.split("\n")
-    for (var i = 0; i < code.length; i++) {
-      code[i] = code[i].trim();
-      code[i] = code[i].replace('//start here', "")
-    }
-    code = code.join('');
-    getApiData(code)
+    await executeUserCode(language, method,code)
+    .then((result)=>{setCodeOutput(result)});
 
   }
 
-  const getApiData = async (code) => {
+  function buildCodeForJava(UserCode) {
+    var code = UserCode.split("\n");
+    for (var i = 0; i < code.length; i++) {
+        code[i] = code[i].trim();
+        code[i] = code[i].replace('//write your code below', "");
+    }
+    code = code.join('');
+    return code;
+}
 
-    const requestOptions = {
-      method: 'POST',
+
+  async function executeUserCode(language, method,code) {
+    const payload = {
+      method: method,
       headers: {
-        'Content-Type': 'text/plain'
+          'Content-Type': 'text/plain',
       },
-      body: JSON.stringify({ "code": code })
-    };
-    await fetch("https://pk8eaiaa0h.execute-api.ap-south-1.amazonaws.com/beta", requestOptions)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          SetCodeOutput(JSON.parse(result.body).message)
-        },
-        (error) => {
-          SetCodeOutput(error.message)
-        }
-      )
   };
+    if (language == "java") {
+        //var javacode = buildCodeForJava(code);
+
+        payload.body=JSON.stringify({"code":buildCodeForJava(code)});
+        return new Promise((resolve, reject) => {
+            httpCall("https://pk8eaiaa0h.execute-api.ap-south-1.amazonaws.com/beta", payload)
+                .then((result) => { console.log("result is    java " + result); resolve(result.body) })
+                .catch((result) => { console.log("result is    java  " + result); resolve(result.body) });
+        });
+    }
+
+    if (language == "javascript") {
+     payload.body= JSON.stringify({ "code": code });
+        return new Promise((resolve, reject) => {
+            httpCall("http://localhost:3005/api/javascript", payload)
+                .then((result) => { console.log("result is   js  " + result); resolve(result.body) })
+                .catch((result) => { console.log("result is  js   " + result); resolve(result.body) });
+        });
+    }
+}
+
 
   return (
     <div className="App">
       {!Loginstatus && <div id="buttonDiv"></div>}
-
       {Loginstatus && <Header />}
-
       {Loginstatus && <Split direction="horizontal" className='main-container'>
         {Loginstatus && <Problems />}
-        {Loginstatus && <CodeEditor UserCode={UserCode} codeChange={codeChange} getOutput={getOutput} />}
+        {Loginstatus && <CodeEditor UserCode={UserCode} codeChange={codeChange} executeCode={executeCode} />}
         {Loginstatus && <OutputWindow CodeOutput={CodeOutput} />}
       </Split>}
       {Loginstatus && <Footer />}
-
     </div>
   );
 }
